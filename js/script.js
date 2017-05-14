7a65a7d7648d0b044990fc97da1c83991c28c20f
halo2008 <ksedkowski@gmail.com>
2017-05-14 20:04:15 +0200

    new code

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function Button(text) {
+	this.text = text || 'Hello';
+}
+
+Button.prototype = {
+    create: function () {
+		var self = this;
+		this.$element = $('Button');
+		this.$element.text(this.text);
+		this.$element.appendTo($('body'));
+		this.$element.click(function(){
+		alert(self.text);
+		});    
+    }
+}
+
+var btn1 = new Button('Hello!');
+btn1.create();
